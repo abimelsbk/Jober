1546a46b5726b8aeac5e919d41d488cdd271c6b7
ABIMEL S B KULUMALA <abimelsbkulumala@gmail.com>
2023-06-24 16:12:19 +0530

    main

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('helo')
+# main.py
+
+from deployment.job_matching import match_jobs
+
+if __name__ == "__main__":
+    job_matches = match_jobs()
+    # Further processing or output generation with job_matches
